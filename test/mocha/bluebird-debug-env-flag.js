2374608380b92c7449f3744e50624ba134277127
Petka Antonov <petka_antonov@hotmail.com>
2013-10-17 07:33:06 +0300

    Check for NodeJS before running the test

--- test/mocha/bluebird-debug-env-flag.js
+++ test/mocha/bluebird-debug-env-flag.js
@@ -3,17 +3,22 @@ var Promise = require('../../js/bluebird');
 
 var assert = require("assert");
 
-describe("buebird-debug-env-flag", function() {
+var isNodeJS = typeof process !== "undefined" && process !== null &&
+    typeof process.execPath === "string";
 
-    it("should enable long stack traces", function(done) {
-        Promise.fulfilled().then(function() {
-            throw new Error("Oops");
-        }).catch(function(err) {
-            process.nextTick(function() {
-                assert(err.stack.indexOf("From previous event") >= 0, 
-                       "env flag should enable long stack traces");
-                done();
+if( isNodeJS ) {
+    describe("buebird-debug-env-flag", function() {
+
+        it("should enable long stack traces", function(done) {
+            Promise.fulfilled().then(function() {
+                throw new Error("Oops");
+            }).catch(function(err) {
+                process.nextTick(function() {
+                    assert(err.stack.indexOf("From previous event") >= 0,
+                           "env flag should enable long stack traces");
+                    done();
+                });
             });
         });
     });
-});
+}
