cfa189dd974aa5117db3e137256859125aeddf8f
Petka Antonov <petka_antonov@hotmail.com>
2013-10-23 00:37:38 +0300

    build

--- js/bluebird_sync.js
+++ js/bluebird_sync.js
@@ -217,6 +217,15 @@ function makeNodePromisifiedEval( callback, receiver, originalName ) {
         "break;";
     }
 
+    function getArgs() {
+        return "var args = new Array( len + 1 );" +
+        "var i = 0;" +
+        "for( var i = 0; i < len; ++i ) { " +
+        "   args[i] = arguments[i];" +
+        "}" +
+        "args[i] = fn;";
+    }
+
     var callbackName = ( typeof originalName === "string" ?
         originalName + "Async" :
         "promisified" );
@@ -236,12 +245,12 @@ function makeNodePromisifiedEval( callback, receiver, originalName ) {
         "case 0:" + getCall(0) +
         "case 4:" + getCall(4) +
         "case 5:" + getCall(5) +
-        "default: " + (typeof callback === "string"
+        "default: " + getArgs() + (typeof callback === "string"
             ? "this['" + callback + "'].apply("
             : "callback.apply("
         ) +
             ( receiver === THIS ? "this" : "receiver" ) +
-        ", withAppended( arguments, fn ) ); break;" +
+        ", args ); break;" +
         "}" +
         "}" +
         "catch(e){ " +
@@ -1650,6 +1659,30 @@ Promise.rejected = function Promise$Rejected( reason ) {
     return ret;
 };
 
+Promise["try"] = Promise.attempt = function Promise$Try( fn, args, ctx ) {
+    var ret = new Promise();
+    ret._setTrace( Promise.attempt, void 0 );
+    ret._cleanValues();
+    if( typeof fn !== "function" ) {
+        ret._setRejected();
+        ret._resolvedValue = new TypeError("fn must be a function");
+        return ret;
+    }
+    var value = isArray( args )
+        ? tryCatchApply( fn, args, ctx )
+        : tryCatch1( fn, ctx, args );
+
+    if( value === errorObj ) {
+        ret._setRejected();
+        ret._resolvedValue = value.e;
+    }
+    else {
+        ret._setFulfilled();
+        ret._resolvedValue = value;
+    }
+    return ret;
+};
+
 Promise.pending = function Promise$Pending( caller ) {
     var promise = new Promise();
     promise._setTrace( typeof caller === "function"
@@ -1673,10 +1706,6 @@ Promise.cast = function Promise$Cast( obj, caller ) {
         return Promise.fulfilled( ret, caller );
     }
     return ret;
-};
-
-Promise["try"] = Promise.attempt = function( fn ) {
-    return Promise.fulfilled().then( fn );
 };
 
 Promise.onPossiblyUnhandledRejection =
@@ -2281,13 +2310,13 @@ function Promise$_assumeStateOf( promise, mustAsync ) {
         );
     }
     else if( promise.isFulfilled() ) {
-        if( mustAsync )
+        if( mustAsync === true )
             this._resolveFulfill(promise._resolvedValue);
         else
             this._resolveFulfill( promise._resolvedValue );
     }
     else {
-        if( mustAsync )
+        if( mustAsync === true )
             this._resolveReject(promise._resolvedValue);
         else
             this._resolveReject( promise._resolvedValue );
