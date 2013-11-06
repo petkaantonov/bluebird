cbe579a51b2807b4ee2f88a646867622421f1941
Petka Antonov <petka_antonov@hotmail.com>
2013-11-06 12:52:30 +0200

    in Promises/A+ 2.2.5 use actual this values

--- test/mocha/2.2.5.js
+++ test/mocha/2.2.5.js
@@ -8,13 +8,22 @@ var rejected = adapter.rejected;
 
 var dummy = { dummy: "dummy" }; // we fulfill or reject with this when we don't intend to test against it
 
+var undefinedThisStrict = (function() {
+    "use strict";
+    return this;
+})();
+
+var undefinedThisSloppy = (function() {
+    return this;
+})();
+
 describe("2.2.5 `onFulfilled` and `onRejected` must be called as functions (i.e. with no `this` value).", function () {
     describe("strict mode", function () {
         specify("fulfilled", function (done) {
             fulfilled(dummy).then(function onFulfilled() {
                 "use strict";
 
-                assert.strictEqual(this, undefined);
+                assert.strictEqual(this, undefinedThisStrict);
                 done();
             });
         });
@@ -23,7 +32,7 @@ describe("2.2.5 `onFulfilled` and `onRejected` must be called as functions (i.e.
             rejected(dummy).then(null, function onRejected() {
                 "use strict";
 
-                assert.strictEqual(this, undefined);
+                assert.strictEqual(this, undefinedThisStrict);
                 done();
             });
         });
@@ -32,14 +41,14 @@ describe("2.2.5 `onFulfilled` and `onRejected` must be called as functions (i.e.
     describe("sloppy mode", function () {
         specify("fulfilled", function (done) {
             fulfilled(dummy).then(function onFulfilled() {
-                assert.strictEqual(this, global);
+                assert.strictEqual(this, undefinedThisSloppy);
                 done();
             });
         });
 
         specify("rejected", function (done) {
             rejected(dummy).then(null, function onRejected() {
-                assert.strictEqual(this, global);
+                assert.strictEqual(this, undefinedThisSloppy);
                 done();
             });
         });
