2432080a6c49f4c4ff141296dac61925ea0a0209
Andreas Lubbe <andreaslubbe@gmail.com>
2013-12-03 16:26:57 -0800

    fixed baseline in doxbee-sequential-errors

--- benchmark/doxbee-sequential-errors/callbacks-baseline.js
+++ benchmark/doxbee-sequential-errors/callbacks-baseline.js
@@ -7,8 +7,8 @@ module.exports = function upload(stream, idOrPath, tag, done) {
         tx.rollback();
         return done(err);
     }
-    var triggerIntentionalError = function(){
-        if(LIKELIHOOD_OF_REJECTION && Math.random() <= LIKELIHOOD_OF_REJECTION) done(new Error("intentional failure"));
+    var intentionalErrorShouldBeTriggered = function(){
+        return LIKELIHOOD_OF_REJECTION && Math.random() <= LIKELIHOOD_OF_REJECTION;
     }
 
     blob.put(stream, function (err, blobId) {
@@ -26,7 +26,7 @@ module.exports = function upload(stream, idOrPath, tag, done) {
             version.id = Version.createHash(version);
             Version.insert(version).execWithin(tx, function (err) {
                 if (err) return backoff(err);
-                triggerIntentionalError();
+                if(intentionalErrorShouldBeTriggered()) return done(new Error("intentional failure"));
                 if (!file) {
                     var splitPath = idOrPath.split('/');
                     var fileName = splitPath[splitPath.length - 1];
@@ -38,7 +38,7 @@ module.exports = function upload(stream, idOrPath, tag, done) {
                         version: version.id
                     }, function (err, q) {
                         if (err) return backoff(err);
-                        triggerIntentionalError();
+                        if(intentionalErrorShouldBeTriggered()) return done(new Error("intentional failure"));
                         q.execWithin(tx, function (err) {
                             afterFileExists(err, newId);
                         });
@@ -49,16 +49,16 @@ module.exports = function upload(stream, idOrPath, tag, done) {
             });
             function afterFileExists(err, fileId) {
                 if (err) return backoff(err);
-                triggerIntentionalError();
+                if(intentionalErrorShouldBeTriggered()) return done(new Error("intentional failure"));
                 FileVersion.insert({fileId: fileId,versionId: version.id})
                     .execWithin(tx, function (err) {
                         if (err) return backoff(err);
-                        triggerIntentionalError();
+                        if(intentionalErrorShouldBeTriggered()) return done(new Error("intentional failure"));
                         File.whereUpdate({id: fileId}, {
                             version: version.id
                         }).execWithin(tx, function (err) {
                             if (err) return backoff(err);
-                            triggerIntentionalError();
+                            if(intentionalErrorShouldBeTriggered()) return done(new Error("intentional failure"));
                             tx.commit(done);
                         });
                 })
