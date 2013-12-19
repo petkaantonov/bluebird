5b07518d9159b30a7bb1c8c0befa4a46ae537229
Mick Hansen <mick.kasper.hansen@gmail.com>
2013-12-19 17:32:07 +0100

    an actual failing test

--- test/mocha/nodeify.js
+++ test/mocha/nodeify.js
@@ -10,11 +10,23 @@ var Promise = adapter;
 
 describe("Promise.nodeify", function(){
 	it("should used resolved results in nodeify callback", function (done) {
-		fulfilled(10).then(function (value) {
-			return value * 2;
-		}).nodeify(function (value) {
+		var defered = Promise.defer();
+
+		defered.promise.then(function (value) {
+			var nested = Promise.defer();
+			
+			setTimeout(function () {
+				nested.resolve(value * 2);
+			}, 10);
+
+			return _defered;
+		}).nodeify(function (err, value) {
 			assert(value === 20);
 			done();
 		});
+
+		setTimeout(function () {
+			defered.resolve(10);
+		}, 10);
 	});
 });
