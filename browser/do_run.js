e9932fb763f618885026fc6cc75991084919c964
Petka Antonov <petka_antonov@hotmail.com>
2014-02-01 12:30:40 +0200

    sauce hacks

--- browser/do_run.js
+++ browser/do_run.js
@@ -1,5 +1,19 @@
 var runner = mocha.run();
 runner.on('end', function() {
+    var stats = runner.stats;
     //Saucelabs idiosyncrasy
-    window.mochaResults = runner.stats
+    stats.failures = stats.tests - stats.passes;
+    window.mochaResults = runner.stats;
 });
+
+var lastTests = 0;
+setInterval(function(){
+    var tests = runner.stats.tests;
+    if (!window.mochaResults && tests === lastTests) {
+        var stats = runner.stats;
+        //Saucelabs idiosyncrasy
+        stats.failures = stats.tests - stats.passes;
+        window.mochaResults = runner.stats;
+    }
+    lastTests = tests;
+}, 2000);
