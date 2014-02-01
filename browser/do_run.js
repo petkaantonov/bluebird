83ac260f25c1aff127be25f7baafd3573c793d08
Petka Antonov <petka_antonov@hotmail.com>
2014-02-01 11:58:44 +0200

    use saucelabs mocha runner

--- browser/do_run.js
+++ browser/do_run.js
@@ -1,11 +1,5 @@
 var runner = mocha.run();
 runner.on('end', function() {
     //Saucelabs idiosyncrasy
-    window.mochaResults = {
-        suites: runner.suite.suites.length,
-        tests: runner.total,
-        passes: runner.total - runner.failures,
-        failures: runner.failures,
-        pending: 0
-    };
+    window.mochaResults = runner.stats
 });
