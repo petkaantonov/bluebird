1bc37e43ed583279bd2b83addd0df0ef67014d7f
Petka Antonov <petka_antonov@hotmail.com>
2014-03-30 20:26:43 +0300

    Custom build dependencies

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,6 @@
 "use strict";
 module.exports = function(Promise, apiRejection) {
+var PromiseMap = Promise._map;
 function filterer(booleans) {
     var values = this;
     var len = values.length;
@@ -12,21 +13,20 @@ function filterer(booleans) {
     return ret;
 }
 
-Promise.prototype.filter = function Promise$filter(fn) {
+function filter(promises, fn, receiver) {
     if (typeof fn !== "function") return apiRejection(NOT_FUNCTION_ERROR);
-    var promiseArray = Promise._map(this, fn, this._boundTo, true);
+    var promiseArray = PromiseMap(promises, fn, receiver, true);
+    var preservedValues = promiseArray.preservedValues();
     return promiseArray
             .promise()
-            ._then(filterer, void 0, void 0,
-                    promiseArray.preservedValues(), void 0);
+            ._then(filterer, void 0, void 0, preservedValues, void 0);
+}
+
+Promise.prototype.filter = function Promise$filter(fn) {
+    return filter(this, fn, this._boundTo);
 };
 
 Promise.filter = function Promise$Filter(promises, fn) {
-    if (typeof fn !== "function") return apiRejection(NOT_FUNCTION_ERROR);
-    var promiseArray = Promise._map(promises, fn, void 0, true);
-    return promiseArray
-            .promise()
-            ._then(filterer, void 0, void 0,
-                    promiseArray.preservedValues(), void 0);
+    return filter(promises, fn, void 0);
 };
 };
