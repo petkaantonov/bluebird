3a33aa05e022c3449c4043d20227349b2bc9bc4d
Petka Antonov <petka_antonov@hotmail.com>
2014-03-31 23:15:12 +0300

    Refactor Promies.props
    - No longer split into 2 files
    - Remove dependency hack in PromiseArray

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,6 @@ module.exports = function( grunt ) {
                     "./src/promise_array.js",
                     "./src/settled_promise_array.js",
                     "./src/some_promise_array.js",
-                    "./src/properties_promise_array.js",
                     "./src/promise_resolver.js",
                     "./src/promise_spawn.js"
                 ]
@@ -529,7 +528,6 @@ module.exports = function( grunt ) {
         "./src/promise_array.js",
         "./src/settled_promise_array.js",
         "./src/some_promise_array.js",
-        "./src/properties_promise_array.js",
         "./src/synchronous_inspection.js",
         "./src/promise_resolver.js",
         "./src/promise_spawn.js",
