e6bd2e014b36e0ab94b7ae204bd7a131e0e2f4f2
Petka Antonov <petka_antonov@hotmail.com>
2014-04-17 23:22:37 +0300

    Fixes #179

--- src/global.js
+++ src/global.js
@@ -1,24 +1,9 @@
-"use strict";
-module.exports = (function(){
-//Not in strict mode
-if (typeof this !== "undefined") {
-    return this;
-}
-//Strict mode, node
-if (typeof process !== "undefined" &&
-    typeof global !== "undefined" &&
-    typeof process.execPath === "string") {
-    return global;
-}
-//Strict mode, browser
-if (typeof window !== "undefined" &&
-    typeof document !== "undefined" &&
-    typeof navigator !== "undefined" && navigator !== null &&
-    typeof navigator.appName === "string") {
-        //Strict mode, Firefox extension
-        if(window.wrappedJSObject !== undefined){
-            return window.wrappedJSObject;
-        }
-    return window;
-}
+module.exports = (function() {
+    if (this !== void 0) return this;
+    try {return global;}
+    catch(e) {}
+    try {return window;}
+    catch(e) {}
+    try {return self;}
+    catch(e) {}
 })();
