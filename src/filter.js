12662ea275a91f9bbaf298429e57de17cd999c12
Petka Antonov <petka_antonov@hotmail.com>
2014-04-20 13:11:39 +0300

    Add concurrency option to .map and .filter

--- src/filter.js
+++ src/filter.js
@@ -2,11 +2,11 @@
 module.exports = function(Promise, INTERNAL) {
 var PromiseMap = Promise.map;
 
-Promise.prototype.filter = function Promise$filter(fn) {
-    return PromiseMap(this, fn, INTERNAL);
+Promise.prototype.filter = function Promise$filter(fn, options) {
+    return PromiseMap(this, fn, options, INTERNAL);
 };
 
-Promise.filter = function Promise$Filter(promises, fn) {
-    return PromiseMap(promises, fn, INTERNAL);
+Promise.filter = function Promise$Filter(promises, fn, options) {
+    return PromiseMap(promises, fn, options, INTERNAL);
 };
 };
--- test/mocha/when_map.js
+++ test/mocha/when_map.js
@@ -240,3 +240,181 @@ describe("when.map-test", function () {
 
     });
 });
+
+describe("when.map-test with concurrency", function () {
+
+    var concurrency = {concurrency: 2};
+
+    function mapper(val) {
+        return val * 2;
+    }
+
+    function deferredMapper(val) {
+        return delay(mapper(val), Math.random()*10);
+    }
+
+    specify("should map input values array with concurrency", function(done) {
+        var input = [1, 2, 3];
+        when.map(input, mapper, concurrency).then(
+            function(results) {
+                assert.deepEqual(results, [2,4,6]);
+                done();
+            },
+            fail
+        );
+    });
+
+    specify("should map input promises array with concurrency", function(done) {
+        var input = [resolved(1), resolved(2), resolved(3)];
+        when.map(input, mapper, concurrency).then(
+            function(results) {
+                assert.deepEqual(results, [2,4,6]);
+                done();
+            },
+            fail
+        );
+    });
+
+    specify("should map mixed input array with concurrency", function(done) {
+        var input = [1, resolved(2), 3];
+        when.map(input, mapper, concurrency).then(
+            function(results) {
+                assert.deepEqual(results, [2,4,6]);
+                done();
+            },
+            fail
+        );
+    });
+
+    specify("should map input when mapper returns a promise with concurrency", function(done) {
+        var input = [1,2,3];
+        when.map(input, deferredMapper, concurrency).then(
+            function(results) {
+                assert.deepEqual(results, [2,4,6]);
+                done();
+            },
+            fail
+        );
+    });
+
+    specify("should accept a promise for an array with concurrency", function(done) {
+        when.map(resolved([1, resolved(2), 3]), mapper, concurrency).then(
+            function(result) {
+                assert.deepEqual(result, [2,4,6]);
+                done();
+            },
+            fail
+        );
+    });
+
+    specify("should resolve to empty array when input promise does not resolve to an array with concurrency", function(done) {
+        when.map(resolved(123), mapper, concurrency).caught(TypeError, function(e){
+            done();
+        });
+    });
+
+    specify("should map input promises when mapper returns a promise with concurrency", function(done) {
+        var input = [resolved(1),resolved(2),resolved(3)];
+        when.map(input, mapper, concurrency).then(
+            function(results) {
+                assert.deepEqual(results, [2,4,6]);
+                done();
+            },
+            fail
+        );
+    });
+
+    specify("should reject when input contains rejection with concurrency", function(done) {
+        var input = [resolved(1), reject(2), resolved(3)];
+        when.map(input, mapper, concurrency).then(
+            fail,
+            function(result) {
+                assert( result === 2 );
+                done();
+            }
+        );
+    });
+
+    specify("should propagate progress with concurrency", function(done) {
+        var input = [1, 2, 3];
+        var donecalls = 0;
+        function donecall() {
+            if( ++donecalls === 3 ) done();
+        }
+
+        when.map(input, function(x) {
+            var d = when.pending();
+            d.progress(x);
+            setTimeout(d.fulfill.bind(d, x), 0);
+            return d.promise;
+        }, concurrency).then(null, null, function(update) {
+            assert(update.value === input.shift());
+            donecall();
+        });
+    });
+
+    specify("should propagate progress 2 with concurrency", function(done) {
+         // Thanks @depeele for this test
+        var input, ncall;
+
+        input = [_resolver(1), _resolver(2), _resolver(3)];
+        ncall = 0;
+
+        function identity(x) {
+            return x;
+        }
+        //This test didn't contain the mapper argument so I assume
+        //when.js uses identity mapper in such cases.
+        //In bluebird it's illegal to call Promise.map without mapper function
+        return when.map(input, identity, concurrency).then(function () {
+            assert(ncall === 6);
+            done();
+        }, fail, function () {
+            ncall++;
+        });
+
+        function _resolver(id) {
+          var p = when.defer();
+
+          setTimeout(function () {
+            var loop, timer;
+
+            loop = 0;
+            timer = setInterval(function () {
+              p.notify(id);
+              loop++;
+              if (loop === 2) {
+                clearInterval(timer);
+                p.resolve(id);
+              }
+            }, 1);
+          }, 0);
+
+          return p.promise;
+        }
+
+    });
+
+
+    specify("should not have more than 5 promises in flight", function(done) {
+        var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        var b = [];
+        var delay = when.delay;
+        var wasEmpty = false;
+        var now = Date.now();
+        when.map(array, function(value, index) {
+            var d = index > 4 ? 0 : 100;
+            return delay(d).then(function() {
+                b.push(value);
+            });
+        }, {concurrency: 5}).then(function() {
+            assert(wasEmpty);
+            assert.deepEqual(b, [0, 1, 2, 3, 4, 10, 9, 8, 7, 6, 5]);
+            done();
+        });
+
+        delay(33).then(function() {
+            wasEmpty = b.length === 0;
+        });
+    });
+});
