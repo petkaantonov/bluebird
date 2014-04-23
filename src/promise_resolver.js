13a36fd5916e078dfe5cc6637ecdfbc66465e244
Brendan Clark <brendan.clark@hbo.com>
2014-04-22 20:01:42 -0700

    Because I can't write correct code on the 1st try

--- src/promise_resolver.js
+++ src/promise_resolver.js
@@ -143,7 +143,7 @@ function PromiseResolver$progress(value) {
  */
 PromiseResolver.prototype.cancel = function PromiseResolver$cancel() {
     var err = new CancellationError();
-    this._attachExtraTrace(err);
+    this.promise._attachExtraTrace(err);
 
     async.invoke(this.promise.cancel, this.promise, err);
 };
