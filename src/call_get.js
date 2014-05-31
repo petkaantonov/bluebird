3f2d64264972eea9a0064d5abfab8eb28973f38e
Petka Antonov <petka_antonov@hotmail.com>
2014-05-31 10:47:40 +0300

    Optimize .call some more

--- src/call_get.js
+++ src/call_get.js
@@ -1,42 +1,59 @@
 "use strict";
+var cache = Object.create && Object.create(null);
+var cacheSize = 0;
 module.exports = function(Promise) {
+var util = require("./util.js");
+var canEvaluate = util.canEvaluate;
+var isIdentifier = util.isIdentifier;
 
-function Promise$caller(obj) {
-    var len = this.length;
-    switch (len) {
-        case 2: return obj[this[1]](this[0]);
-        case 3: return obj[this[2]](this[0], this[1]);
-        case 4: return obj[this[3]](this[0], this[1], this[2]);
-        case 1: return obj[this[0]]();
-        default: return obj[this.pop()].apply(obj, this);
-    }
+function makeMethodCaller (methodName) {
+    return new Function("obj", "                                               \
+        var len = this.length;                                                 \
+        switch(len) {                                                          \
+            case 1: return obj. " + methodName + "(this[0]);                   \
+            case 2: return obj. " + methodName + "(this[0], this[1]);          \
+            case 3: return obj. " + methodName + "(this[0], this[1], this[2]); \
+            case 0: return obj. " + methodName + "();                          \
+            default: return obj. " + methodName + ".apply(obj, this);          \
+        }                                                                      \
+        ");
 }
 
+function getMethodCaller(name) {
+    var ret = cache[name];
+    if (typeof ret !== "function") {
+        if (!isIdentifier(name)) {
+            return null;
+        }
+        ret = makeMethodCaller(name);
+        cache[name] = ret;
+        if (cacheSize > 512) {
+            var keys = Object.keys(cache);
+            for (var i = 0; i < 256; ++i) delete cache[keys[i]];
+        }
+    }
+    return ret;
+}
+
+function Promise$caller(obj) {
+    return obj[this.pop()].apply(obj, this);
+}
 Promise.prototype.call = function Promise$call(methodName) {
     INLINE_SLICE(args, arguments, 1);
+    if (canEvaluate) {
+        var maybeCaller = getMethodCaller(methodName);
+        if (maybeCaller !== null) {
+            return this._then(maybeCaller, void 0, void 0, args, void 0);
+        }
+    }
     args.push(methodName);
-    return this._then(
-        Promise$caller,
-        void 0,
-        void 0,
-        args,
-        void 0
-   );
+    return this._then(Promise$caller, void 0, void 0, args, void 0);
 };
 
 function Promise$getter(obj) {
-    var prop = typeof this === "string"
-        ? this
-        : ("" + this);
-    return obj[prop];
+    return obj[this];
 }
 Promise.prototype.get = function Promise$get(propertyName) {
-    return this._then(
-        Promise$getter,
-        void 0,
-        void 0,
-        propertyName,
-        void 0
-   );
+    return this._then(Promise$getter, void 0, void 0, propertyName, void 0);
 };
 };
