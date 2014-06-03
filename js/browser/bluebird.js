908e26452a055af56878206a27f3c13213b672ac
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-06-03 02:52:20 -0300

    Fix broken tests

--- js/browser/bluebird.js
+++ js/browser/bluebird.js
@@ -1764,14 +1764,14 @@ function Promise$catch(fn) {
 Promise.prototype.then =
 function Promise$then(didFulfill, didReject, didProgress) {
     return this._then(didFulfill, didReject, didProgress,
-        this, void 0);
+        void 0, void 0);
 };
 
 
 Promise.prototype.done =
 function Promise$done(didFulfill, didReject, didProgress) {
     var promise = this._then(didFulfill, didReject, didProgress,
-        this, void 0);
+        void 0, void 0);
     promise._setIsFinal();
 };
 
@@ -2038,7 +2038,10 @@ function Promise$_then(
         async.invoke(this._queueSettleAt, this, callbackIndex);
     }
 
-    ret._parent = this;
+    if (ret) {
+      ret._parent = this;
+    }
+
     return ret;
 };
 
@@ -4647,19 +4650,19 @@ module.exports = function(Promise) {
 
 Promise.prototype.tearDown =
 function Promise$tearDown(callback) {
-  this._tearDownResolver = callback;
-  return this;
+    this._tearDownResolver = callback;
+    return this;
 };
 
 Promise.prototype._tearDown =
 function Promise$_tearDown() {
-  if (this._tearDownResolver) {
-    this._tearDownResolver.call(this._boundTo);
-  }
+    if (this._tearDownResolver) {
+        this._tearDownResolver.call(this._boundTo);
+    }
 
-  if (this._parent) {
-    this._parent._tearDown();
-  }
+    if (this._parent) {
+        this._parent._tearDown();
+    }
 };
 
 };
