464dae270df477dd44f98d18d94322cb7888dd0d
Daniel Foley <daniel.foley@dena.com>
2014-06-25 10:06:10 -0700

    refactor the Promises$reduce state machine

--- src/reduce.js
+++ src/reduce.js
@@ -5,32 +5,37 @@ var tryCatch4 = util.tryCatch4;
 var tryCatch3 = util.tryCatch3;
 var errorObj = util.errorObj;
 var PENDING = {};
-// -2=The initial current index when no initial value is given
-// -1=The initial current index when initial value is given as a pending promise
-// 0=The initial current index when initial value is given as an immediate value
 function ReductionPromiseArray(promises, fn, accum, _each) {
     this.constructor$(promises);
-    var currentIndex = -2;
     this._preservedValues = _each === INTERNAL ? [] : null;
+    // `false` = initialValue provided as `accum` argument
+    // `true` = treat the 0th value as initialValue
+    this._zerothIsAccum = (accum === void 0);
+    // `true` = we've received our initialValue
+    this._gotAccum = false;
+    // phase 1 = we've received its initial value
+    // phase 2 = we've received its reduced value
+    this._valuesPhase = [];
+    // index of the value we are in the process of reducing
+    this._reducingIndex = (this._zerothIsAccum ? 1 : 0);
+
     var maybePromise = cast(accum, void 0);
     var rejected = false;
     var isPromise = maybePromise instanceof Promise;
     if (isPromise) {
         if (maybePromise.isPending()) {
-            currentIndex = -1;
             maybePromise._proxyPromiseArray(this, -1);
         } else if (maybePromise.isFulfilled()) {
             accum = maybePromise.value();
-            currentIndex = 0;
+            this._gotAccum = true;
         } else {
             maybePromise._unsetRejectionIsUnhandled();
             this._reject(maybePromise.reason());
             rejected = true;
         }
     }
-    if (!isPromise && accum !== void 0) currentIndex = 0;
+    if (! (isPromise || this._zerothIsAccum)) this._gotAccum = true;
     this._callback = fn;
-    this._currentIndex = currentIndex;
     this._accum = accum;
     if (!rejected) this._init$(void 0, RESOLVE_CALL_METHOD);
 }
@@ -43,10 +48,10 @@ function ReductionPromiseArray$_init() {};
 // Override
 ReductionPromiseArray.prototype._resolveEmptyArray =
 function ReductionPromiseArray$_resolveEmptyArray() {
-    // If current index is -1, the initial value
-    // is still a pending promise, which is handled in
-    // promiseFulfilled in the -1 case
-    if (this._currentIndex !== -1) {
+    // we may resolve
+    // once we've received our initialValue,
+    // or if it's the 0th value (and we don't need to wait)
+    if (this._gotAccum || this._zerothIsAccum) {
         this._resolve(this._preservedValues !== null
                         ? [] : this._accum);
     }
@@ -55,52 +60,61 @@ function ReductionPromiseArray$_resolveEmptyArray() {
 // Override
 ReductionPromiseArray.prototype._promiseFulfilled =
 function ReductionPromiseArray$_promiseFulfilled(value, index) {
-    var accum;
+    // not really fulfilled
+    if (value === PENDING) return;
     var values = this._values;
     if (values === null) return;
     var length = this.length();
-    var currentIndex = this._currentIndex;
-    // Already processed
-    if (currentIndex > index) return;
     var preservedValues = this._preservedValues;
     var isEach = preservedValues !== null;
+    var gotAccum = this._gotAccum;
+    var valuesPhase = this._valuesPhase;
+
     // Special case detection where the processing starts at index 1
     // because no initialValue was given
-    if (index === 0 && currentIndex === -2) {
-        accum = value;
-        currentIndex = 1;
-        if (length < 2) return this._resolve(void 0);
-        value = values[1];
-    // Cannot process this index right now, process it later
-    } else if (index > currentIndex) {
-        return;
-    // Round 2 promise or the initialValue was a promise and was fulfilled
-    } else if (index === -1 || values[index] === PENDING) {
-        accum = value;
-        currentIndex++;
-        if (currentIndex >= length)
-            return this._resolve(isEach ? preservedValues : accum);
-        value = values[currentIndex];
+    if (index === 0 && this._zerothIsAccum) {
+        if (! gotAccum) {
+            this._accum = value;
+            this._gotAccum = gotAccum = true;
+        }
+        values[index] = value;
+        valuesPhase[index] = (valuesPhase[index] || 0) + 1;
+    // the initialValue was a promise and was fulfilled
+    } else if (index === -1) {
+        if (! gotAccum) {
+            this._accum = value;
+            this._gotAccum = gotAccum = true;
+        }
     } else {
-        accum = this._accum;
+        values[index] = value;
+        valuesPhase[index] = (valuesPhase[index] || 0) + 1;
+        if (gotAccum && (valuesPhase[index] > 1)) {
+            this._accum = value;
+        }
     }
+    // no point in reducing anything until we have an initialValue
+    if (! gotAccum) return;
 
     var callback = this._callback;
     var receiver = this._promise._boundTo;
     var ret;
 
-    for (var i = currentIndex; i < length; ++i) {
-        // The first value is always set in above code, only later values
-        // will be read from the array
-        if (i > currentIndex) value = values[i];
+    for (var i = this._reducingIndex; i < length; ++i) {
+        var phase = valuesPhase[i];
+        if (! phase) return;
+        if (phase > 1) {
+            // it has already been reduced
+            this._reducingIndex = i + 1;
+            continue;
+        }
 
+        value = values[i];
+        if (value === PENDING) return;
         if (value instanceof Promise) {
             if (value.isFulfilled()) {
                 value = value._settledValue;
             } else if (value.isPending()) {
                 // Continue later when the promise at current index fulfills
-                this._accum = accum;
-                this._currentIndex = i;
                 return;
             } else {
                 value._unsetRejectionIsUnhandled();
@@ -113,7 +127,7 @@ function ReductionPromiseArray$_promiseFulfilled(value, index) {
             ret = tryCatch3(callback, receiver, value, i, length);
         }
         else {
-            ret = tryCatch4(callback, receiver, accum, value, i, length);
+            ret = tryCatch4(callback, receiver, this._accum, value, i, length);
         }
 
         if (ret === errorObj) return this._reject(ret.e);
@@ -123,10 +137,8 @@ function ReductionPromiseArray$_promiseFulfilled(value, index) {
             // Callback returned a pending
             // promise so continue iteration when it fulfills
             if (maybePromise.isPending()) {
-                // Round 2 marker
+                // Phase 2 marker
                 values[i] = PENDING;
-                this._accum = accum;
-                this._currentIndex = i;
                 return maybePromise._proxyPromiseArray(this, i);
             } else if (maybePromise.isFulfilled()) {
                 ret = maybePromise.value();
@@ -135,9 +147,14 @@ function ReductionPromiseArray$_promiseFulfilled(value, index) {
                 return this._reject(maybePromise.reason());
             }
         }
-        accum = ret;
+
+        this._reducingIndex = i + 1;
+        this._accum = ret;
     }
-    this._resolve(isEach ? preservedValues : accum);
+
+    // end-game, once everything has been resolved
+    if (this._reducingIndex < length) return;
+    this._resolve(isEach ? preservedValues : this._accum);
 };
 
 function reduce(promises, fn, initialValue, _each) {
--- test/mocha/reduce.js
+++ test/mocha/reduce.js
@@ -15,6 +15,18 @@ function promised(val) {
         }, 4);
     });
 }
+function promising(val) {
+    return function() {
+        return promised(val);
+    }
+}
+function promisingThen(val) {
+    return function() {
+        return promised(val).then(function(resolved) {
+            return resolved;
+        });
+    }
+}
 
 function thenabled(val) {
     return {
@@ -25,9 +37,113 @@ function thenabled(val) {
         }
     };
 }
+function thenabling(val) {
+    return function() { return thenabled(val); }
+}
+
+function evaluate(val) {
+    if (typeof val === 'function') {
+        val = val();
+    }
+    if (Array.isArray(val)) {
+        val = val.map(function(member) {
+            return evaluate(member);
+        });
+    }
+    return val;
+}
+
+
+var ACCUM_CRITERIA = [
+    { value: 0,                desc: "that is resolved" },
+    { value: promising(0),     desc: "as a Promise" },
+    { value: promisingThen(0), desc: "as a deferred Promise" },
+    { value: thenabling(0),    desc: "as a thenable" },
+];
+
+var VALUES_CRITERIA = [
+    { value: [],               total: 0, desc: "and no values" },
+    { value: [ 1 ],            total: 1, desc: "and a single resolved value" },
+    { value: [ 1, 2, 3 ],      total: 6, desc: "and multiple resolved values" },
+    { value: [ promising(1) ], total: 1, desc: "and a single Promise" },
+    { value: [
+        promising(1),
+        promising(2),
+        promising(3)
+    ], total: 6, desc: "and multiple Promises" },
+    { value: [
+        promisingThen(1)
+    ], total: 1, desc: "and a single deferred Promise" },
+    { value: [
+        promisingThen(1),
+        promisingThen(2),
+        promisingThen(3),
+    ], total: 6, desc: "and multiple deferred Promises" },
+    { value: [
+        thenabling(1)
+    ], total: 1, desc: "and a single thenable" },
+    { value: [
+        thenabling(1),
+        thenabling(2),
+        thenabling(3)
+    ], total: 6, desc: "and multiple thenables" },
+    { value: [
+        thenabling(1),
+        promisingThen(2),
+        promising(3),
+        4,
+    ], total: 10, desc: "and a blend of values" },
+];
+
+var ERROR = new Error("BOOM");
+
 
 describe("Promise.prototype.reduce", function() {
+    it("works with no values", function(done) {
+        Promise.resolve([]).reduce(function(total, value) {
+            return total + value + 5;
+        }).then(function(total) {
+            assert.strictEqual(total, undefined);
+        }).nodeify(done);
+    });
 
+    it("works with a single value", function(done) {
+        Promise.resolve([ 1 ]).reduce(function(total, value) {
+            return total + value + 5;
+        }).then(function(total) {
+            assert.strictEqual(total, 1);
+        }).nodeify(done);
+    });
+
+    it("works when the iterator returns a value", function(done) {
+        Promise.resolve([ 1, 2, 3 ]).reduce(function(total, value) {
+            return total + value + 5;
+        }).then(function(total) {
+            assert.strictEqual(total, (1 + 2+5 + 3+5));
+        }).nodeify(done);
+    });
+
+    it("works when the iterator returns a Promise", function(done) {
+        Promise.resolve([ 1, 2, 3 ]).reduce(function(total, value) {
+            return promised(5).then(function(bonus) {
+                return total + value + bonus;
+            });
+        }).then(function(total) {
+            assert.strictEqual(total, (1 + 2+5 + 3+5));
+        }).nodeify(done);
+    });
+
+    it("works when the iterator returns a thenable", function(done) {
+        Promise.resolve([ 1, 2, 3 ]).reduce(function(total, value) {
+            return thenabled(total + value + 5);
+        }).then(function(total) {
+            assert.strictEqual(total, (1 + 2+5 + 3+5));
+        }).nodeify(done);
+    });
+});
+
+
+describe("Promise.reduce", function() {
 
     it("should allow returning values", function(done) {
         var a = [promised(1), promised(2), promised(3)];
@@ -78,6 +194,243 @@ describe("Promise.prototype.reduce", function() {
         }, 0).then(assert.fail, function(err) {
             assert.equal(err, e);
             done();
+        });
+    });
+
+    describe("with no initial accumulator or values", function() {
+        it("works when the iterator returns a value", function(done) {
+            return Promise.reduce([], function(total, value) {
+                return total + value + 5;
+            }).then(function(total){
+                assert.strictEqual(total, undefined);
+            }).nodeify(done);
+        });
+
+        it("works when the iterator returns a Promise", function(done) {
+            return Promise.reduce([], function(total, value) {
+                return promised(5).then(function(bonus) {
+                    return total + value + bonus;
+                });
+            }).then(function(total){
+                assert.strictEqual(total, undefined);
+            }).nodeify(done);
+        });
+
+        it("works when the iterator returns a thenable", function(done) {
+            return Promise.reduce([], function(total, value) {
+                return thenabled(total + value + 5);
+            }).then(function(total){
+                assert.strictEqual(total, undefined);
+            }).nodeify(done);
+        });
+    });
+
+    describe("with an initial accumulator value", function() {
+        ACCUM_CRITERIA.forEach(function(criteria) {
+            var initial = criteria.value;
+
+            describe(criteria.desc, function() {
+                VALUES_CRITERIA.forEach(function(criteria) {
+                    var values = criteria.value;
+                    var valueTotal = criteria.total;
+
+                    describe(criteria.desc, function() {
+                        it("works when the iterator returns a value", function(done) {
+                            return Promise.reduce(evaluate(values), function(total, value) {
+                                return total + value + 5;
+                            }, evaluate(initial)).then(function(total){
+                                assert.strictEqual(total, valueTotal + (values.length * 5));
+                            }).nodeify(done);
+                        });
+
+                        it("works when the iterator returns a Promise", function(done) {
+                            return Promise.reduce(evaluate(values), function(total, value) {
+                                return promised(5).then(function(bonus) {
+                                    return total + value + bonus;
+                                });
+                            }, evaluate(initial)).then(function(total){
+                                assert.strictEqual(total, valueTotal + (values.length * 5));
+                            }).nodeify(done);
+                        });
+
+                        it("works when the iterator returns a thenable", function(done) {
+                            return Promise.reduce(evaluate(values), function(total, value) {
+                                return thenabled(total + value + 5);
+                            }, evaluate(initial)).then(function(total){
+                                assert.strictEqual(total, valueTotal + (values.length * 5));
+                            }).nodeify(done);
+                        });
+                    });
+                });
+            });
+        });
+
+        it("propagates an initial Error", function(done) {
+            var initial = Promise.reject(ERROR);
+            var values = [
+                thenabling(1),
+                promisingThen(2)(),
+                promised(3),
+                4
+            ];
+
+            Promise.reduce(values, function(total, value) {
+                return value;
+            }, initial).then(assert.fail, function(err) {
+                assert.equal(err, ERROR);
+                done();
+            });
+        });
+
+        it("propagates a value's Error", function(done) {
+            var initial = 0;
+            var values = [
+                thenabling(1),
+                promisingThen(2)(),
+                Promise.reject(ERROR),
+                promised(3),
+                4
+            ];
+
+            Promise.reduce(values, function(total, value) {
+                return value;
+            }, initial).then(assert.fail, function(err) {
+                assert.equal(err, ERROR);
+                done();
+            });
+        });
+
+        it("propagates an Error from the iterator", function(done) {
+            var initial = 0;
+            var values = [
+                thenabling(1),
+                promisingThen(2)(),
+                promised(3),
+                4
+            ];
+
+            Promise.reduce(values, function(total, value) {
+                if (value === 2) {
+                    throw ERROR;
+                }
+                return value;
+            }, initial).then(assert.fail, function(err) {
+                assert.equal(err, ERROR);
+                done();
+            });
+        });
+    });
+
+    describe("with a 0th value acting as an accumulator", function() {
+        it("acts this way when an accumulator value is provided yet `undefined`", function(done) {
+            return Promise.reduce([ 1, 2, 3 ], function(total, value) {
+                return ((total === void 0) ? 0 : total) + value + 5;
+            }, undefined).then(function(total){
+                assert.strictEqual(total, (1 + 2+5 + 3+5));
+            }).nodeify(done);
+        });
+
+        it("survives an `undefined` 0th value", function(done) {
+            return Promise.reduce([ undefined, 1, 2, 3 ], function(total, value) {
+                return ((total === void 0) ? 0 : total) + value + 5;
+            }).then(function(total){
+                assert.strictEqual(total, (1+5 + 2+5 + 3+5));
+            }).nodeify(done);
+        });
+
+        ACCUM_CRITERIA.forEach(function(criteria) {
+            var zeroth = criteria.value;
+
+            describe(criteria.desc, function() {
+                VALUES_CRITERIA.forEach(function(criteria) {
+                    var values = criteria.value;
+                    var zerothAndValues = [ zeroth ].concat(values);
+                    var valueTotal = criteria.total;
+
+                    describe(criteria.desc, function(done) {
+                        it("works when the iterator returns a value", function(done) {
+                            return Promise.reduce(evaluate(zerothAndValues), function(total, value) {
+                                return total + value + 5;
+                            }).then(function(total){
+                                assert.strictEqual(total, valueTotal + (values.length * 5));
+                            }).nodeify(done);
+                        });
+
+                        it("works when the iterator returns a Promise", function(done) {
+                            return Promise.reduce(evaluate(zerothAndValues), function(total, value) {
+                                return promised(5).then(function(bonus) {
+                                    return total + value + bonus;
+                                });
+                            }).then(function(total){
+                                assert.strictEqual(total, valueTotal + (values.length * 5));
+                            }).nodeify(done);
+                        });
+
+                        it("works when the iterator returns a thenable", function(done) {
+                            return Promise.reduce(evaluate(zerothAndValues), function(total, value) {
+                                return thenabled(total + value + 5);
+                            }).then(function(total){
+                                assert.strictEqual(total, valueTotal + (values.length * 5));
+                            }).nodeify(done);
+                        });
+                    });
+                });
+            });
+        });
+
+        it("propagates an initial Error", function(done) {
+            var values = [
+                Promise.reject(ERROR),
+                thenabling(1),
+                promisingThen(2)(),
+                promised(3),
+                4
+            ];
+
+            Promise.reduce(values, function(total, value) {
+                return value;
+            }).then(assert.fail, function(err) {
+                assert.equal(err, ERROR);
+                done();
+            });
+        });
+
+        it("propagates a value's Error", function(done) {
+            var values = [
+                0,
+                thenabling(1),
+                promisingThen(2)(),
+                Promise.reject(ERROR),
+                promised(3),
+                4
+            ];
+
+            Promise.reduce(values, function(total, value) {
+                return value;
+            }).then(assert.fail, function(err) {
+                assert.equal(err, ERROR);
+                done();
+            });
+        });
+
+        it("propagates an Error from the iterator", function(done) {
+            var values = [
+                0,
+                thenabling(1),
+                promisingThen(2)(),
+                promised(3),
+                4
+            ];
+
+            Promise.reduce(values, function(total, value) {
+                if (value === 2) {
+                    throw ERROR;
+                }
+                return value;
+            }).then(assert.fail, function(err) {
+                assert.equal(err, ERROR);
+                done();
+            });
         });
     });
 });
