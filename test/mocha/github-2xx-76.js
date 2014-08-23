1433d3379014c115636cf0d472fb085c5397b4a7
Petka Antonov <petka_antonov@hotmail.com>
2014-08-23 14:02:21 +0300

    Add nodejs guard for nodejs test

--- test/mocha/github-2xx-76.js
+++ test/mocha/github-2xx-76.js
@@ -4,15 +4,18 @@
 var Promise = require("../../js/debug/bluebird.js");
 Promise.longStackTraces();
 var assert = require("assert");
+var isNodeJS = typeof process !== "undefined" &&
+    typeof process.execPath === "string";
 
-describe("github276 - stack trace cleaner", function(){
-    specify("message with newline and a$_b should not be removed", function(done){
-        Promise.resolve(1).then(function() {
-            throw new Error("Blah\n          a$_b");
-        }).catch(function(e) {
-            var msg = e.stack.split('\n')[1]
-            assert(msg.indexOf('a$_b') >= 0, 'message should contain a$_b');
-        }).done(done, done);
+if (isNodeJS) {
+    describe("github276 - stack trace cleaner", function(){
+        specify("message with newline and a$_b should not be removed", function(done){
+            Promise.resolve(1).then(function() {
+                throw new Error("Blah\n          a$_b");
+            }).caught(function(e) {
+                var msg = e.stack.split('\n')[1]
+                assert(msg.indexOf('a$_b') >= 0, 'message should contain a$_b');
+            }).done(done, done);
+        });
     });
-});
-
+}
