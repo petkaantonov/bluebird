65776d62f957d6bd6a8e067497fcbe4a46411fff
Petka Antonov <petka_antonov@hotmail.com>
2015-01-27 03:00:07 +0200

    Safari

--- tools/browser_test_runner.js
+++ tools/browser_test_runner.js
@@ -16,6 +16,8 @@ var browsers = [
     ["Windows 7", "chrome", "beta"],
     ["Windows 7", "safari", "5"],
     ["OS X 10.9", "iphone", "8.1"],
+    ["OS X 10.8", "safari", "6"],
+    ["OS X 10.9", "safari", "7"]
 ];
 
 var saucelabsOptions = {
