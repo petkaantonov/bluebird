87aa7a3866ffad9aeb83af5ff18783931305f8af
Petka Antonov <petka_antonov@hotmail.com>
2015-02-16 16:12:48 +0200

    Provide alias mapSeries for each

--- src/each.js
+++ src/each.js
@@ -2,11 +2,11 @@
 module.exports = function(Promise, INTERNAL) {
 var PromiseReduce = Promise.reduce;
 
-Promise.prototype.each = function (fn) {
+Promise.prototype.mapSeries = Promise.prototype.each = function (fn) {
     return PromiseReduce(this, fn, INTERNAL, INTERNAL);
 };
 
-Promise.each = function (promises, fn) {
+Promise.mapSeries = Promise.each = function (promises, fn) {
     return PromiseReduce(promises, fn, INTERNAL, INTERNAL);
 };
 };
