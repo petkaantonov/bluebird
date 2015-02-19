9e05fa2f05dae7743951e0e6774e911a9a647814
Petka Antonov <petka_antonov@hotmail.com>
2015-02-19 22:35:27 +0200

    Deprecate Promise.try with more than 1 argument

--- src/method.js
+++ src/method.js
@@ -1,6 +1,6 @@
 "use strict";
 module.exports =
-function(Promise, INTERNAL, tryConvertToPromise, apiRejection) {
+function(Promise, INTERNAL, tryConvertToPromise, apiRejection, debug) {
 var util = require("./util.js");
 var ASSERT = require("./assert.js");
 var tryCatch = util.tryCatch;
@@ -27,7 +27,16 @@ Promise.attempt = Promise["try"] = function (fn) {
     var ret = new Promise(INTERNAL);
     ret._captureStackTrace();
     ret._pushContext();
-    var value = tryCatch(fn)();
+    var value;
+    if (arguments.length > 1) {
+        debug.deprecated("calling Promise.try with more than 1 argument");
+        var arg = arguments[1];
+        var ctx = arguments[2];
+        value = util.isArray(arg) ? tryCatch(fn).apply(ctx, arg)
+                                  : tryCatch(fn).call(ctx, arg);
+    } else {
+        value = tryCatch(fn)();
+    }
     ret._popContext();
     ret._resolveFromSyncValue(value);
     return ret;
