e719003498d056e708c77775a3015d39b0949582
Benoit Marchant <marchant@mac.com>
2015-02-20 00:17:10 -0800

    Reverts MessageChannel based scheduler based on petkaantonov’s feedback, will use custom setScheduler instead

--- src/schedule.js
+++ src/schedule.js
@@ -5,23 +5,6 @@ if (require("./util.js").isNode) {
     schedule = (version[0] === 0 && version[1] > 10) || (version[0] > 0)
         ? global.setImmediate : process.nextTick;
 }
-else if (typeof MessageChannel !== "undefined") {
-    schedule = (function() {
-        var queuedFn = void 0;
-        // modern browsers
-        // http://www.nonblocking.io/2011/06/windownexttick.html
-        var channel = new MessageChannel();
-        // At least Safari Version 6.0.5 (8536.30.1) intermittently cannot 
-        // create working message ports the first time a page loads.
-        channel.port1.onmessage = function _scheduleExec() {
-            queuedFn();
-        };
-        return function _schedulePost(fn)  {
-            queuedFn = fn;
-            channel.port2.postMessage(0);
-        };
-    })();
-}
 else if (typeof MutationObserver !== "undefined") {
     schedule = function(fn) {
         var div = document.createElement("div");
