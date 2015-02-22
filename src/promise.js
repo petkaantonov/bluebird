9f0867c8134547d40074465cb7623dbd797caa91
Petka Antonov <petka_antonov@hotmail.com>
2015-02-22 04:05:29 +0200

    Remove ws

--- src/promise.js
+++ src/promise.js
@@ -680,8 +680,8 @@ Promise.prototype._settlePromises = function () {
     this._setLength(0);
 };
 
-function deferResolve(v) {this.promise._resolveCallback(v);}
-function deferReject(v) {this.promise._rejectCallback(v, false);}
+function deferResolve(v) {this.promise._resolveCallback(v);}
+function deferReject(v) {this.promise._rejectCallback(v, false);}
 
 Promise.defer = function() {
     debug.deprecated("Promise.defer", "new Promise");
