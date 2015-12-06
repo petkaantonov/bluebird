28ab4a27e81c17d680e3b7301082f10aa496b6e4
Alexander Bolshakov <alexander.bolshakov@sap.com>
2015-12-06 14:58:58 +0200

    Added monitoring functionality for promisses
    This change introduces monitoring for pending promises. Promises are registred at creation and unregistred when settled. At any moment libarary user can see all pending promises by calling Promise.getAllPendingPromises() or just the last promises in all chains by calling Promise.getLeafPendingPromises(). This basic implementation can be extended to show the bigger picture by adding bi-directional connections between promises and building DOT graph of all promises that currently exist.
    
    Discussion in mailing list: https://groups.google.com/forum/#!topic/bluebird-js/PMQBfVqCSnA

---
+++ test/mocha/monitor.js
@@ -0,0 +1,129 @@
+var assert = require("assert");
+
+// In this test each promise created in this test should be explicitly marked,
+// not to mix it up with promises created by Mocha
+
+describe("promises monitoring", function() {
+
+	Promise.monitor();
+
+	function assertPendingPromises(expectedPromisesArray, getPendingPromisesFunctionName) {
+		assert(typeof Promise[getPendingPromisesFunctionName] === "function");
+		var allPendingPromises = Promise[getPendingPromisesFunctionName]();
+		// Remove all promises not related to this test
+		var i;
+		for (i = 0; i < allPendingPromises.length; i++) {
+			if (!allPendingPromises[i].test) {
+				allPendingPromises.splice(i,1);
+				i--;
+			}
+		}
+		assert(allPendingPromises.length === expectedPromisesArray.length);
+		for (i = 0; i < allPendingPromises.length; i++) {
+			assert(allPendingPromises[i] === expectedPromisesArray[i]);
+		}
+	}
+
+	function assertAllAndLeafPendingPromises(expectedPromisesArray) {
+		assertPendingPromises(expectedPromisesArray, "getAllPendingPromises");
+		assertPendingPromises(expectedPromisesArray, "getLeafPendingPromises");
+	}
+
+	function deferAndMarkAsTestPromise() {
+		var resolve, reject;
+		var promise = new Promise(function() {
+			resolve = arguments[0];
+			reject = arguments[1];
+		});
+		promise.test = true;
+		return {
+			resolve: resolve,
+			reject: reject,
+			promise: promise
+		};
+	}
+	
+	afterEach( function() {
+		var allPromises = Promise.getAllPendingPromises();
+		for (var i = 0; i < allPromises.length; i++) {
+			delete allPromises[i];   
+		}
+	});
+
+	it("promises added to monitor array after creation and removed after resolution", function() {
+		var deferred = deferAndMarkAsTestPromise();
+		assertAllAndLeafPendingPromises([deferred.promise]);
+		var lastPromise = deferred.promise.then(function() {
+			assertAllAndLeafPendingPromises([]);
+		});
+		deferred.resolve();
+		return lastPromise;
+	});
+
+	it("promises added to monitor array after creation and removed after rejection", function() {
+		var deferred = deferAndMarkAsTestPromise();
+		assertAllAndLeafPendingPromises([deferred.promise]);
+		var lastPromise = deferred.promise.catch(function() {
+			assertAllAndLeafPendingPromises([]);
+		});
+		deferred.reject(new Error("reason"));
+		return lastPromise;
+	});
+
+	it("promises added to monitor array after creation and removed after direct resolution", function() {
+		var deferred = deferAndMarkAsTestPromise();
+		assertAllAndLeafPendingPromises([deferred.promise]);
+		deferred.resolve();
+		var lastPromise = deferred.promise.thenReturn();
+		assertAllAndLeafPendingPromises([]);
+		return lastPromise;
+	});
+
+	it("promises added to monitor array after creation and removed after direct rejection", function() {
+		var deferred = deferAndMarkAsTestPromise();
+		assertAllAndLeafPendingPromises([deferred.promise]);
+		deferred.resolve();
+		var lastPromise = deferred.promise.thenThrow(new Error("reason")).catch(function (){});
+		assertAllAndLeafPendingPromises([]);
+		return lastPromise;
+	});
+
+	it("leaves of promises chains are calculated correctly", function() {
+		// A <- B <- C (Leaf)
+		//     /|\
+		//      D <- E (Leaf)
+		//
+		// F <- G (Leaf)
+		var Adeferred = deferAndMarkAsTestPromise();
+		var A = Adeferred.promise;
+		A.test = true;
+		var B = A.then(function() {});
+		B.test = true;
+		var C = B.then(function() {});
+		C.test = true;
+		var D = B.then(function() {});
+		D.test = true;
+		var E = D.then(function() {});
+		E.test = true;
+		var Fdeferred = deferAndMarkAsTestPromise();
+		var F = Fdeferred.promise;
+		F.test = true;
+		var G = F.then(function() {});
+		G.test = true;
+
+		assertPendingPromises([A, B, C, D, E, F, G], "getAllPendingPromises");
+		assertPendingPromises([C, E, G], "getLeafPendingPromises");
+
+		var lastPromise = Promise.all([C, E]).then(function() {
+			assertPendingPromises([F, G], "getAllPendingPromises");
+			assertPendingPromises([G], "getLeafPendingPromises");
+			G.catch(function() {
+				assertPendingPromises([], "getAllPendingPromises");
+				assertPendingPromises([], "getLeafPendingPromises");
+			});
+			Fdeferred.reject();
+		});
+		Adeferred.resolve();
+		return lastPromise;
+	});
+});
