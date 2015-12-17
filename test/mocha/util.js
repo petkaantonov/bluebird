4e93ea94e1e31b3b17f387600007daf0c05e7640
Alexander Bolshakov <alexander.bolshakov@sap.com>
2015-12-17 15:36:36 +0200

    This change introduces monitoring for pending promises. Promises are registred at creation and unregistred when their fate is sealed. At any moment library user can see all pending promises by calling Promise.getPendingPromises() or just the last promises in all chains by calling Promise.getLeafPendingPromises().

---
+++ test/mocha/util.js
@@ -0,0 +1,52 @@
+"use strict";
+var assert = require("assert");
+var util = require("../../js/debug/util");
+
+
+describe("utilities", function() {
+
+    var hookExtension = function() {
+        hookExtension.executedFlag = true;
+    };
+
+    var hookExtension1 = function() {
+        hookExtension1.executedFlag = true;
+    };
+
+    beforeEach(function () {
+        if (Promise.prototype.hook) delete Promise.prototype.hook;
+        hookExtension.executedFlag = false;
+        hookExtension1.executedFlag = false;
+    });
+
+    it("hookTo function adds handler to a hook", function() {
+        util.hookTo(Promise.prototype,"hook", hookExtension);
+        var promise = new Promise(function (){});
+        assert (promise.hook.extensions[0].executedFlag === false);
+        promise.hook();
+        assert (promise.hook.extensions[0].executedFlag === true);
+    });
+
+    it("hookTo function adds 2 handlers to a hook", function() {
+        util.hookTo(Promise.prototype,"hook", hookExtension);
+        util.hookTo(Promise.prototype,"hook", hookExtension1);
+        var promise = new Promise(function (){});
+        assert (promise.hook.extensions[0].executedFlag === false);
+        assert (promise.hook.extensions[1].executedFlag === false);
+        promise.hook();
+        assert (promise.hook.extensions[0].executedFlag === true);
+        assert (promise.hook.extensions[1].executedFlag === true);
+    });
+
+    it("unhookFrom function removes handler from a hook", function() {
+        util.hookTo(Promise.prototype,"hook", hookExtension);
+        util.unhookFrom(Promise.prototype,"hook", hookExtension);
+        var promise = new Promise(function (){});
+        assert(Promise.prototype["hook"].extensions.length === 0);
+        assert(promise.hook.extensions.length === 0);
+    });
+
+    after(function () {
+        delete Promise.prototype.hook;
+    });
+});
