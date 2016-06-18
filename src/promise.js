8d63aa105435e6bbe14c7c5388230b83ac9d8442
overlookmotel <theoverlookmotel@gmail.com>
2016-06-18 02:05:07 +0100

    getNewLibraryCopy method

--- src/promise.js
+++ src/promise.js
@@ -164,6 +164,8 @@ Promise.prototype.error = function (fn) {
     return this.caught(util.originatesFromRejection, fn);
 };
 
+Promise.getNewLibraryCopy = module.exports;
+
 Promise.is = function (val) {
     return val instanceof Promise;
 };
