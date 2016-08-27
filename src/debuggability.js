petkaantonov/bluebird

commit e820d8a7a592179519323ca533280f011a43d27b

Fixes #1190

src/debuggability.js
@@ -143,14 +143,24 @@ Promise.hasLongStackTraces = function () {
 
 var fireDomEvent = (function() {
     try {
-        var event = document.createEvent("CustomEvent");
-        event.initCustomEvent("testingtheevent", false, true, {});
-        util.global.dispatchEvent(event);
-        return function(name, event) {
-            var domEvent = document.createEvent("CustomEvent");
-            domEvent.initCustomEvent(name.toLowerCase(), false, true, event);
-            return !util.global.dispatchEvent(domEvent);
-        };
+        if (typeof CustomEvent === "function") {
+            var event = new CustomEvent("CustomEvent");
+            util.global.dispatchEvent(event);
+            return function(name, event) {
+                var domEvent = new CustomEvent(name.toLowerCase(), event);
+                return !util.global.dispatchEvent(domEvent);
+            };
+        } else {
+            var event = document.createEvent("CustomEvent");
+            event.initCustomEvent("testingtheevent", false, true, {});
+            util.global.dispatchEvent(event);
+            return function(name, event) {
+                var domEvent = document.createEvent("CustomEvent");
+                domEvent.initCustomEvent(name.toLowerCase(), false, true,
+                    event);
+                return !util.global.dispatchEvent(domEvent);
+            };
+        }
     } catch (e) {}
     return function() {
         return false;

commit dd2f3dda1c1e1f154ae544242fdd3eb4d798b71e

Set cancelable property on CustomEvent.
Commit e820d8a7a592179519323ca533280f011a43d27b introduced a couple of
regressions:

1. Details not set properly. This was fixed by commit
288aa285fe61d4a9c360253eb5cbb66d04a81c62.

2. It also failed to properly initialize the cancelable property. This
commit fixes it.

src/debuggability.js
@@ -152,7 +152,8 @@ var fireDomEvent = (function() {
             util.global.dispatchEvent(event);
             return function(name, event) {
                 var domEvent = new CustomEvent(name.toLowerCase(), {
-                    detail: event
+                    detail: event,
+                    cancelable: true
                 });
                 return !util.global.dispatchEvent(domEvent);
             };