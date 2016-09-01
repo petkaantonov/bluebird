6211fa1062880525f3ad39fba92e0f5d53287cc8
Benjamin Gruenbaum <inglor@gmail.com>
2016-09-01 12:04:04 +0300

    fix tests, attempt to fix predicates

--- test/mocha/tapCatch.js
+++ test/mocha/tapCatch.js
@@ -1,20 +1,26 @@
 "use strict";
 var assert = require("assert");
 var testUtils = require("./helpers/util.js");
+function rejection() {
+    var error = new Error("test");
+    var rejection = Promise.reject(error);
+    rejection.err = error;
+    return rejection;
+}
 
+describe("tapError", function () {
 
-describe("tap", function () {
     specify("passes through rejection reason", function() {
-        return Promise.reject("test").tapError(function() {
+        return rejection().tapError(function() {
             return 3;
         }).caught(function(value) {
-            assert.equal(value, "test");
+            assert.equal(value.message, "test");
         });
     });
 
     specify("passes through reason after returned promise is fulfilled", function() {
         var async = false;
-        return Promise.reject("test").tapError(function() {
+        return rejection().tapError(function() {
             return new Promise(function(r) {
                 setTimeout(function(){
                     async = true;
@@ -23,7 +29,7 @@ describe("tap", function () {
             });
         }).caught(function(value) {
             assert(async);
-            assert.equal(value, "test");
+            assert.equal(value.message, "test");
         });
     });
 
@@ -38,7 +44,7 @@ describe("tap", function () {
 
     specify("passes immediate rejection", function() {
         var err = new Error();
-        return Promise.reject("test").tapError(function() {
+        return rejection().tapError(function() {
             throw err;
         }).tap(assert.fail).then(assert.fail, function(e) {
             assert(err === e);
@@ -47,7 +53,7 @@ describe("tap", function () {
 
     specify("passes eventual rejection", function() {
         var err = new Error();
-        return Promise.reject("test").tapError(function() {
+        return rejection().tapError(function() {
             return new Promise(function(_, rej) {
                 setTimeout(function(){
                     rej(err);
@@ -59,8 +65,8 @@ describe("tap", function () {
     });
 
     specify("passes reason", function() {
-        return Promise.reject(123).tapError(function(a) {
-            assert(a === 123);
+        return rejection().tapError(function(a) {
+            assert(a === rejection);
         }).then(assert.fail, function() {});
     });
 
@@ -68,16 +74,18 @@ describe("tap", function () {
         var called = false;
         return Promise.reject(new TypeError).tapError(TypeError, function(a) {
             called = true;
-        }).then(assert.fail, function() { 
+        }).then(assert.fail, function(err) { 
             assert(called === true);
+            assert(err instanceof TypeError);
         });
     });
     specify("Does not get called on predicates that don't match ", function() {
         var called = false;
         return Promise.reject(new TypeError).tapError(ReferneceError, function(a) {
             called = true;
-        }).then(assert.fail, function() { 
+        }).then(assert.fail, function(err) { 
             assert(called === false);
+            assert(err instanceof ReferenceError);
         });
     });
 });
