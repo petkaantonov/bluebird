0566325cf1001ea4d4de4554a952a93eeef4c447
Gil Tayar <gilt@createfuture.co.il>
2017-01-27 14:57:09 +0200

    Fixed #1314:  yielding a function should not call the function (#1315)
    * Fixed #1314:  yielding a function should not call the function
       When rejecting a non-promise value, string.replace is used,
        and the value is passed as second parameter.
        But string.replace, if the second value is a function, will
        call that function.
        This commit fixes this by using value.toString().
    
    * changed how value is converted to string in error about yielding non-promises

--- src/generators.js
+++ src/generators.js
@@ -160,7 +160,7 @@ PromiseSpawn.prototype._continue = function (result) {
             if (maybePromise === null) {
                 this._promiseRejected(
                     new TypeError(
-                        YIELDED_NON_PROMISE_ERROR.replace("%s", value) +
+                        YIELDED_NON_PROMISE_ERROR.replace("%s", String(value)) +
                         FROM_COROUTINE_CREATED_AT +
                         this._stack.split("\n").slice(1, -7).join("\n")
                     )
