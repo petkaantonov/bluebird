fae85eb601d08cccf38dc59044f25c646c33d6f9
Dan Levy <dan@danlevy.net>
2017-09-14 12:51:59 -0600

    passing initial tests

--- src/conditional.js
+++ src/conditional.js
@@ -1,47 +1,66 @@
-'use strict';
+"use strict";
+/* jshint -W098 */
 
 module.exports = function _conditionals(Promise) {
   Promise.prototype.thenIf = thenIf;
   Promise.thenIf = _thenIf;
 
   function thenIf() {
-    var cond = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : function (x) {
-      return x;
-    };
+    var cond =
+      arguments.length > 0 && arguments[0] !== undefined
+        ? arguments[0]
+        : function(x) {
+            return x;
+          };
 
     var _this = this;
 
-    var ifTrue = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function (x) {
-      return x;
-    };
-    var ifFalse = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function (x) {
-      return null;
-    };
+    var ifTrue =
+      arguments.length > 1 && arguments[1] !== undefined
+        ? arguments[1]
+        : function(x) {
+            return x;
+          };
+    var ifFalse =
+      arguments.length > 2 && arguments[2] !== undefined
+        ? arguments[2]
+        : function(x) {
+            return null;
+          };
 
     if (this instanceof Promise) {
-      return this.then(function (value) {
+      return this.then(function(value) {
         return _thenIf(_this, cond, ifTrue, ifFalse)(value);
       });
     } else if (cond instanceof Function) {
       return _thenIf(this, cond, ifTrue, ifFalse);
     } else {
-      throw new Error('Error calling `thenIf` - Try `this` must be a Promise');
+      throw new Error("Error calling `thenIf` - Try `this` must be a Promise");
     }
   }
 
   function _thenIf(promise) {
-    var cond = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function (x) {
-      return x;
-    };
-    var ifTrue = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function (x) {
-      return x;
-    };
-    var ifFalse = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : function (x) {
-      return null;
-    };
+    var cond =
+      arguments.length > 1 && arguments[1] !== undefined
+        ? arguments[1]
+        : function(x) {
+            return x;
+          };
+    var ifTrue =
+      arguments.length > 2 && arguments[2] !== undefined
+        ? arguments[2]
+        : function(x) {
+            return x;
+          };
+    var ifFalse =
+      arguments.length > 3 && arguments[3] !== undefined
+        ? arguments[3]
+        : function(x) {
+            return null;
+          };
 
-    return function (value) {
-      return Promise.resolve(cond(value)).then(function (ans) {
+    return function(value) {
+      return Promise.resolve(cond(value)).then(function(ans) {
         return ans ? ifTrue(value) : ifFalse(value);
       });
     };
